8146ade5d7841f747bb583ebfe8ed1b89963d133
Roman Kozko <romanka2001@mail.ru>
2020-04-07 22:09:07 +0300

    feat:add main picture,start animation and song

---
+++ Unit1.pas
@@ -0,0 +1,251 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.MPlayer,
+  Vcl.Imaging.jpeg;
+
+type
+  TForm1 = class(TForm)
+    timerProcess: TTimer;
+    MediaPlayer1: TMediaPlayer;
+    Image1: TImage;
+    procedure FormCreate(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure timerProcessTimer(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+var
+  x, y: integer;
+  k: real;
+  i: integer;
+  P, Q: real;
+
+const
+  head = 20;
+
+const
+  legs1 = 10;
+
+const
+  arms1 = 5;
+
+procedure dancer_body(a, b: integer; k: real; canvas: TCanvas);
+begin
+  with canvas do
+  begin
+    ellipse(round((a - head) * k * P), round((b - head * 2) * k * Q),
+      round((a + head) * k * P), round(b * k * Q));
+    moveto(round(a * k * P), round(b * k * Q));
+    lineto(round(a * k * P), round((b + head * 3) * k * Q));
+  end;
+end;
+
+procedure dancer_legs1(a, b: integer; k: real; canvas: TCanvas);
+begin
+  with canvas do
+  begin
+    moveto(round(a * k * P), round((b + 6 * legs1) * k * Q));
+    lineto(round((a + 2 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+    lineto(round((a + 3 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+    moveto(round(a * k * P), round((b + 60) * k * Q));
+    lineto(round((a - 2 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+    lineto(round((a - 3 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+  end;
+end;
+
+procedure dancer_arms1(a, b: integer; k: real; canvas: TCanvas);
+begin
+  with canvas do
+  begin
+    moveto(round(a * k * P), round((b + 4 * arms1) * k * Q));
+    lineto(round((a + 5 * arms1) * k * P), round((b + 8 * arms1) * k * Q));
+    lineto(round((a + 5 * arms1) * k * P), round((b + 11 * arms1) * k * Q));
+    moveto(round(a * k * P), round((b + 20) * k * Q));
+    lineto(round((a - 5 * arms1) * k * P), round((b + 8 * arms1) * k * Q));
+    lineto(round((a - 5 * arms1) * k * P), round((b + 11 * arms1) * k * Q));
+  end;
+end;
+
+procedure dancer_legs2(a, b: integer; k: real; canvas: TCanvas);
+begin
+  with canvas do
+  begin
+    moveto(round(a * k * P), round((b + 6 * legs1) * k * Q));
+    lineto(round((a + 2 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+    lineto(round((a + 3 * legs1) * k * P), round((b + 9 * legs1) * k * Q));
+    moveto(round(a * k * P), round((b + 60) * k * Q));
+    lineto(round((a - 2 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+    lineto(round((a - 3 * legs1) * k * P), round((b + 9 * legs1) * k * Q));
+  end;
+end;
+
+procedure dancer_arms2(a, b: integer; k: real; canvas: TCanvas);
+begin
+  with canvas do
+  begin
+    moveto(round(a * k * P), round((b + 4 * arms1) * k * Q));
+    lineto(round((a + 5 * arms1) * k * P), round((b + arms1) * k * Q));
+    lineto(round((a + 6 * arms1) * k * P), round((b - 3 * arms1) * k * Q));
+    moveto(round(a * k * P), round((b + 20) * k * Q));
+    lineto(round((a - 5 * arms1) * k * P), round((b + arms1) * k * Q));
+    lineto(round((a - 6 * arms1) * k * P), round((b - 3 * arms1) * k * Q));
+  end;
+end;
+
+procedure dancer_legs3(a, b: integer; k: real; canvas: TCanvas);
+begin
+  with canvas do
+  begin
+    moveto(round(a * k * P), round((b + 6 * legs1) * k * Q));
+    lineto(round((a + 2 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+    lineto(round((a + 3 * legs1) * k * P), round((b + 9 * legs1) * k * Q));
+    moveto(round(a * k * P), round((b + 60) * k * Q));
+    lineto(round((a - 2 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+    lineto(round((a - 3 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+  end;
+end;
+
+procedure dancer_arms3(a, b: integer; k: real; canvas: TCanvas);
+begin
+  with canvas do
+  begin
+    moveto(round(a * k * P), round((b + 4 * arms1) * k * Q));
+    lineto(round((a + 5 * arms1) * k * P), round((b + arms1) * k * Q));
+    lineto(round((a + 6 * arms1) * k * P), round((b - 3 * arms1) * k * Q));
+    moveto(round(a * k * P), round((b + 20) * k * Q));
+    lineto(round((a - 5 * arms1) * k * P), round((b + 8 * arms1) * k * Q));
+    lineto(round((a) * k * P), round((b + 11 * arms1) * k * Q));
+  end;
+end;
+
+procedure dancer_legs4(a, b: integer; k: real; canvas: TCanvas);
+begin
+  with canvas do
+  begin
+    moveto(round(a * k * P), round((b + 6 * legs1) * k * Q));
+    lineto(round((a + 2 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+    lineto(round((a + 3 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+    moveto(round(a * k * P), round((b + 60) * k * Q));
+    lineto(round((a - 2 * legs1) * k * P), round((b + 10 * legs1) * k * Q));
+    lineto(round((a - 3 * legs1) * k * P), round((b + 9 * legs1) * k * Q));
+  end;
+end;
+
+procedure dancer_arms4(a, b: integer; k: real; canvas: TCanvas);
+begin
+  with canvas do
+  begin
+    moveto(round(a * k * P), round((b + 20) * k * Q));
+    lineto(round((a + 5 * arms1) * k * P), round((b + 8 * arms1) * k * Q));
+    lineto(round((a) * k * P), round((b + 11 * arms1) * k * Q));
+    moveto(round(a * k * P), round((b + 20) * k * Q));
+    lineto(round((a - 5 * arms1) * k * P), round((b + arms1) * k * Q));
+    lineto(round((a - 6 * arms1) * k * P), round((b - 3 * arms1) * k * Q));
+  end;
+end;
+
+procedure dancer_arms_circule1(a, b: integer; k: real; canvas: TCanvas);
+begin
+  with canvas do
+  begin
+    moveto(round(a * k * P), round((b + 4 * arms1) * k * Q));
+    lineto(round((a + 7 * arms1) * k * P), round((b + 1 * arms1) * k * Q));
+    lineto(round((a + 8 * arms1) * k * P), round((b + 1 * arms1) * k * Q));
+    moveto(round(a * k * P), round((b + 20) * k * Q));
+    lineto(round((a - 7 * arms1) * k * P), round((b + 1 * arms1) * k * Q));
+    lineto(round((a - 8 * arms1) * k * P), round((b + 1 * arms1) * k * Q));
+  end;
+end;
+
+procedure TForm1.FormActivate(Sender: TObject);
+begin
+  canvas.Pen.Width := 4;
+  x := 350;
+  y := 250;
+  k := 1.25;
+  i := 1;
+  timerProcess.Interval := 260;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  MediaPlayer1.FileName := 'C:\Users\Lenovo\Desktop\1231\1.mp3';
+  MediaPlayer1.Open;
+  MediaPlayer1.Play;
+end;
+
+procedure TForm1.timerProcessTimer(Sender: TObject);
+begin
+  P := ClientWidth / 800; // width
+  Q := ClientHeight / 600;
+  with canvas do
+  begin
+    Repaint;
+    DoubleBuffered := True;
+  end;
+  case i of
+    1:
+      begin
+        dancer_body(x, y, k, canvas);
+        dancer_legs1(x, y, k, canvas);
+        dancer_arms1(x, y, k, canvas);
+        inc(i);
+      end;
+    2:
+      begin
+        dancer_body(x, y, k, canvas);
+        dancer_legs2(x, y, k, canvas);
+        dancer_arms1(x, y, k, canvas);
+        inc(i);
+      end;
+    3:
+      begin
+        dancer_body(x, y, k, canvas);
+        dancer_legs2(x, y, k, canvas);
+        dancer_arms2(x, y, k, canvas);
+        inc(i);
+      end;
+    4:
+      begin
+        dancer_body(x, y, k, canvas);
+        dancer_legs3(x, y, k, canvas);
+        dancer_arms3(x, y, k, canvas);
+        inc(i);
+      end;
+    5:
+      begin
+        dancer_body(x, y, k, canvas);
+        dancer_legs4(x, y, k, canvas);
+        dancer_arms4(x, y, k, canvas);
+        inc(i);
+      end;
+    6:
+      begin
+        dancer_body(x, y, k, canvas);
+        dancer_legs1(x, y, k, canvas);
+        dancer_arms_circule1(x, y, k, canvas);
+        i:=1;
+      end;
+    7:
+      begin
+
+      end;
+  end;
+end;
+
+end.
